a7d4f3df148e125a5c77531e2120c12d541e13ee
William Teuschel Klein <williamklein08@gmail.com>
2023-05-19 18:35:00 -0300

    Addition of JS on the page for creating the task and validating the login in the app

--- js/main.js
+++ js/main.js
@@ -1,12 +1,122 @@
 var categoryButtons = document.querySelectorAll('button[name="categories"]');
-function clearCategorySelection() {
-	for (var i = 0; i < categoryButtons.length; i++) {
-		categoryButtons[i].classList.remove("selected");
-	}
-}
-for (var i = 0; i < categoryButtons.length; i++) {
-	categoryButtons[i].addEventListener("click", function () {
+
+categoryButtons.forEach(function (button) {
+	button.addEventListener("click", function () {
 		clearCategorySelection();
 		this.classList.add("selected");
 	});
+});
+
+function clearCategorySelection() {
+	categoryButtons.forEach(function (button) {
+		button.classList.remove("selected");
+	});
+}
+
+function categoryValue() {
+	var selectedButton = document.querySelector(
+		'button[name="categories"].selected'
+	);
+	if (selectedButton) {
+		return selectedButton.innerText.trim();
+	}
+	return "";
+}
+
+function salvarTask(title, date, startTime, endTime, category, description) {
+	var task = localStorage.getItem("Tasks");
+	if (task) {
+		task = JSON.parse(task);
+	} else {
+		task = [];
+	}
+	var novaTask = {
+		title: title,
+		date: date,
+		startTime: startTime,
+		endTime: endTime,
+		category: category,
+		description: description,
+	};
+
+	task.push(novaTask);
+	var taskJSON = JSON.stringify(task);
+	localStorage.setItem("Tasks", taskJSON);
+}
+
+function saveTask() {
+	const title = document.getElementById("titleId").value;
+	const date = document.getElementById("dateId").value;
+	const startTime = document.getElementById("startId").value;
+	const endTime = document.getElementById("endId").value;
+	const description = document.getElementById("descriptionId").value;
+	const category = categoryValue();
+
+	if (title === "") {
+		alert("Insira um título");
+	} else if (date === "") {
+		alert("Insira uma data");
+	} else if (startTime === "") {
+		alert("Insira um tempo de início");
+	} else if (endTime === "") {
+		alert("Insira um tempo de fim");
+	} else if (category === "") {
+		alert("Selecione uma categoria");
+	} else {
+		salvarTask(title, date, startTime, endTime, category, description);
+	}
+}
+
+//FUNÇÃO PARA VERIFICAR OS DADOS
+
+function verificarLogin() {
+	var usuarioLogado = localStorage.getItem("usuarioLogado");
+
+	if (!usuarioLogado && window.location.pathname !== "/login.html") {
+		window.location.href = "login.html";
+	} else if (usuarioLogado && window.location.pathname === "/login.html") {
+		window.location.href = "app.html";
+	}
+}
+
+function fazerLogin() {
+	localStorage.setItem("usuarioLogado", "true");
+	window.location.href = "app.html";
+}
+
+function fazerLogout() {
+	localStorage.removeItem("usuarioLogado");
+	window.location.href = "Login.html";
+}
+
+window.addEventListener("load", verificarLogin);
+
+function verificarCredenciais(email, senha) {
+	var usuarios = localStorage.getItem("Usuarios");
+	if (usuarios) {
+		usuarios = JSON.parse(usuarios);
+
+		for (var i = 0; i < usuarios.length; i++) {
+			var usuario = usuarios[i];
+			if (usuario.email === email && usuario.senha === senha) {
+				console.log("Credenciais válidas. Usuário encontrado:", usuario);
+				fazerLogin();
+				return true;
+			}
+		}
+		alert("Email ou senha incorretos");
+	} else {
+		alert("Usuário não cadastrado");
+	}
+}
+
+function login() {
+	const email = document.getElementById("email").value;
+	const password = document.getElementById("password").value;
+
+	verificarCredenciais(email, password);
+}
+
+function redirecionarParaHome() {
+	window.location.href = "app.html";
 }
